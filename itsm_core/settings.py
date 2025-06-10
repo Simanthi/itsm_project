f7f6b185c8139e2eb3d3ca245ec51677c37397f8
Admin <Admin@DESKTOP-V8V73KN>
2025-06-10 18:32:14 +0530

    commit

--- itsm_core/settings.py
+++ itsm_core/settings.py
@@ -127,6 +127,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+APPEND_SLASH = False  # Ensures URLs are appended with a slash
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
